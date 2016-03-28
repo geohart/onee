17d539fa291def431b69697cb874ffb678615739
geohart <george@sidelime.com>
2016-03-28 15:33:04 -0400

    remove old views

--- routes/index.js
+++ routes/index.js
@@ -14,24 +14,4 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'ONEE' });
 });
 
-/* GET signin page */
-router.get('/signin', function(req, res) {
-  res.render('signin', { title: 'ONEE - Sign in please' });
-});
-
-/* GET signup page */
-router.get('/signup', function(req, res) {
-  res.render('signup', { title: 'ONEE - Sign Up!' });
-});
-
-/* GET connect page */
-router.get('/connect', function(req, res) {
-  res.render('connect', { title: 'ONEE - Connect with a friend' });
-});
-
-/* GET connection page */
-router.get('/view', function(req, res) {
-  res.render('connection', { title: 'ONEE - You\'re connected' });
-});
-
 module.exports = router;
